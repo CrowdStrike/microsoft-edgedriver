c3f51f67d0117d984c8b79caeee2773f1656fee6
Kelly Selden <kellyselden@gmail.com>
2022-10-03 13:06:15 -0700

    chore: add failing test for version mismatch
    If you installed with a different version than you are running with, you are going to have a bad time.

--- test/acceptance/msedgedriver-test.js
+++ test/acceptance/msedgedriver-test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { describe, it } = require('../helpers/mocha');
+const { describe, it, setUpObjectReset } = require('../helpers/mocha');
 const { expect } = require('../helpers/chai');
 const execa = require('execa');
 const path = require('path');
@@ -11,11 +11,39 @@ const binPath = require.resolve('../../bin/msedgedriver');
 describe(path.basename(binPath), function() {
   this.timeout(30e3);
 
+  setUpObjectReset(process.env);
+
   before(async function() {
     await execa.node(installerPath);
   });
 
   it('works', async function() {
+    let ps = execa.node(binPath);
+
+    let isSuccess = false;
+
+    ps.stdout.on('data', data => {
+      let stdout = data.toString();
+
+      if (stdout.includes('Microsoft Edge WebDriver was started successfully.')) {
+        ps.kill();
+
+        isSuccess = true;
+      }
+    });
+
+    await expect(ps).to.eventually.be.rejectedWith('Command was killed with SIGTERM');
+
+    expect(isSuccess).to.equal(true);
+  });
+
+  it('can find bin even if different version', async function() {
+    let version = '102.0.1245.33';
+
+    Object.assign(process.env, {
+      EDGEDRIVER_VERSION: version,
+    });
+
     let ps = execa.node(binPath);
 
     let isSuccess = false;
