90e34de33d3379b5d90f0ccc48f58186b70f9775
Kelly Selden <kellyselden@gmail.com>
2023-01-07 18:54:17 -0800

    chore: ignore broken lint rule

--- src/index.js
+++ src/index.js
@@ -117,6 +117,7 @@ async function getDetectedDriverVersion() {
 }
 
 async function getLatestDriverVersion() {
+  // eslint-disable-next-line node/no-missing-import
   const { got } = await import('got');
 
   let { body } = await got.get(`${downloadHost}/LATEST_STABLE`);
@@ -193,6 +194,7 @@ async function download({ tmpPath, version }) {
 
   console.log(`Downloading ${downloadUrl}...`);
 
+  // eslint-disable-next-line node/no-missing-import
   const { got } = await import('got');
 
   await pipeline(
